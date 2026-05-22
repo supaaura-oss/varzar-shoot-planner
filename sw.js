9d796d3f0d0349d4ec21c222ca7fde21133e83c1
supaaura-oss <supaaura@gmail.com>
2026-05-22 09:26:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,19 @@
-const cacheName = "varzar-shoot-planner-v2";
-const appShell = [
-  "./",
-  "./index.html",
-  "./styles.css?v=2",
-  "./app.js?v=2",
-  "./manifest.webmanifest?v=2",
-  "./icon.svg?v=2",
-];
+const staleCaches = ["varzar-shoot-planner-v1", "varzar-shoot-planner-v2"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appShell)));
+  self.skipWaiting();
+  event.waitUntil(Promise.resolve());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))),
+      .then((keys) => Promise.all(keys.filter((key) => staleCaches.includes(key)).map((key) => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: "window" }))
+      .then((clients) => Promise.all(clients.map((client) => client.navigate(client.url)))),
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-});
+self.addEventListener("fetch", () => {});
